98242ff7c59abfd737cdbea047583497a460326e
sp0k4club <71983183+sp0k4club@users.noreply.github.com>
2026-08-13 20:44:00 +0700

    Update cmsb_lowmem.py

--- cmsb_lowmem.py
+++ cmsb_lowmem.py
@@ -9,7 +9,9 @@ from Queue import Queue
 from threading import Thread, Lock
 
 OUTPUT_DIR = "cms"
-HEADERS = {"User-Agent": "Linux Mozilla 7.0"}
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
 MAX_BODY_BYTES = 262144
 QUEUE_SIZE = 1000
 print_lock = Lock()
